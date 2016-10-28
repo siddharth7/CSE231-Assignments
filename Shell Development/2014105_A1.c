84d7cec26a510b84e52154e325f2a88537294cc6
Mansi Goel <mansi14062@iiitd.ac.in>
2016-10-28 22:44:01 +0530

    shell and filesystem cleaned

--- Shell Development/2014105_A1.c
+++ Shell Development/2014105_A1.c
@@ -16,36 +16,32 @@ void execute_pipes(char *line)
 	pid_t lpid;
 	int ini, ter;
 	int i;
-	//printf("%s\n", line);
+
 	char *newline[100];
 	char *array_arg[100][100];
-		//printf("input hai ye %s\n",cp);
+
 		for (i = 0;i<100; i++) {
 			newline[i]=strtok(line, delimit);
 			 if (newline[i]==NULL)
 			 	break;
             line=NULL;
-            //printf("%s\n",newline[i]);
+
         }
         int x=1,y=0;
         for(i=0;newline[i]!=NULL;i++)
         {
-        	//printf("%s\n",newline[i]);
+
         	if(strcmp(&newline[i][0],"|")==0)
         	{
         		array_arg[x-1][y]=NULL;
         		y=0;
         		x++;
-        		//printf("sdfsdfsf\n");
         	}
         	else
         	{
         		array_arg[x-1][y]=newline[i];
-        		//printf("%d %d %s\n",x-1,y, array_arg[x-1][y]);
-        		y++;
-        		
+        		y++;	
         	}
-
         }
         for(i=0;i<x;i++)
         {
@@ -85,28 +81,27 @@ void execute_pipes(char *line)
         	}
         	else 
         	{ 
-                    if(ini) 
+                if(ini) 
+                {
+                    close(pipe_fd[1]);
+                    ppipe_r = pipe_fd[0];
+                } 
+                else if(ter) 
+                {
+                    close(pipe_fd[0]);
+                    while(wait(&status)!=lpid) 
                     {
-                        close(pipe_fd[1]);
-                        ppipe_r = pipe_fd[0];
-                    } 
-                    else if(ter) 
-                    {
-                        close(pipe_fd[0]);
-                        while(wait(&status)!=lpid) 
-                        {
 
-                        } 
-                        fflush(stdout);
                     } 
-                    else 
-                    { 
-                        close(pipe_fd[1]);
-                        close(ppipe_r);
-                        ppipe_r = pipe_fd[0];
-                    }
+                    fflush(stdout);
+                } 
+                else 
+                { 
+                    close(pipe_fd[1]);
+                    close(ppipe_r);
+                    ppipe_r = pipe_fd[0];
+                }
         	}
-
 		}
 
 }
@@ -121,7 +116,6 @@ char *read_cmd_line()
 		if(character==EOF || character=='\n')
 		{
 			input_line[pointer]='\0';
-			//break;
 			return input_line;
 		}
 		else
@@ -130,22 +124,21 @@ char *read_cmd_line()
 		}
 		pointer++;
 	}
-
 }
+
 void sighandle(int num)
 {
 	char cwd[1000];
 	fprintf(stderr, "\n");
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
-       			{
-       				fprintf(stderr, "%s >", cwd);
-       			}
-       			//printf("ssdsdsfs\n");
+		{
+			fprintf(stderr, "%s >", cwd);
+		}
 	return;
 }
+
 int main()
 {
-	//char *line;
 	char *line;
 	char linec[500];
 	int i;
@@ -154,33 +147,30 @@ int main()
 	char *dirr;
 	signal(SIGINT,sighandle);
 	do{
-			char cwd[1000];
-   			if (getcwd(cwd, sizeof(cwd)) != NULL)
-       			{
-       				fprintf(stdout, "%s >", cwd);
-       				if(flag==1)
-       					{
-       						strcpy(basedir,cwd);
-       						flag=0;
-       						dirr=basedir;
-       						strcat(basedir,"/hist.txt");
-       						//printf("%s\n",basedir);
-       					}
-       			}
-   			else
-       			perror("error");
-       		line = read_cmd_line();
-       		if(strlen(line)==0)
-       		{
+		char cwd[1000];
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			fprintf(stdout, "%s >", cwd);
+			if(flag==1)
+			{
+				strcpy(basedir,cwd);
+				flag=0;
+				dirr=basedir;
+				strcat(basedir,"/hist.txt");
+			}
+		}
 
-       		}
-       		else
-       		{
-       		FILE *fp = fopen(dirr,"a");
-       		fprintf(fp, "%s\n", line);
-       		fclose(fp);
-       		//linec=line;
-       		strcpy(linec,line);
+		else
+			perror("error");
+
+   		line = read_cmd_line();
+
+   		if(strlen(line)!=0)
+   		{
+	   		FILE *fp = fopen(dirr,"a");
+	   		fprintf(fp, "%s\n", line);
+	   		fclose(fp);
+	   		strcpy(linec,line);
 			char *pch;
 			char *line_new[100];
 			int pointer2=0;
@@ -197,7 +187,6 @@ int main()
 			while(pch != NULL)
 			{
 			    strcpy(line_new[i], pch);
-			    //printf ("%s\n",line_new[i]);
 			    if(strcmp(line_new[i],"|")==0)
 			    	number_of_pipes++;
 			    else if(strcmp(line_new[i],">")==0)
@@ -211,7 +200,6 @@ int main()
 			line_new[i] = NULL;
 			if(number_of_pipes)
 			{
-				//printf("pipes detected ;)\n");
 				execute_pipes(linec);
 			}
 			else if(number_of_in && number_of_out)
@@ -220,37 +208,30 @@ int main()
 				int status1;
 				char *newline[100];
 				char *array_arg[100][100];
-					//printf("input hai ye %s\n",cp);
 				char *line=linec;
-					for (i = 0;i<100; i++) {
-						newline[i]=strtok(line, delimit);
-						 if (newline[i]==NULL)
-						 	break;
-			            line=NULL;
-			            //printf("%s\n",newline[i]);
-			        }
-			        int x=1,y=0;
-			        for(i=0;newline[i]!=NULL;i++)
-			        {
-			        	//printf("%s\n",newline[i]);
-			        	if(strcmp(&newline[i][0],">")==0 || strcmp(&newline[i][0],"<")==0)
-			        	{
-			        		array_arg[x-1][y]=NULL;
-			        		y=0;
-			        		x++;
-			        		//printf("sdfsdfsf\n");
-			        	}
-			        	else
-			        	{
-			        		array_arg[x-1][y]=newline[i];
-			        		//printf("%d %d %s\n",x-1,y, array_arg[x-1][y]);
-			        		y++;
-			        		
-			        	}
+				for (i = 0;i<100; i++) {
+					newline[i]=strtok(line, delimit);
+					 if (newline[i]==NULL)
+					 	break;
+		            line=NULL;
+		        }
+		        int x=1,y=0;
+		        for(i=0;newline[i]!=NULL;i++)
+		        {
+		        	if(strcmp(&newline[i][0],">")==0 || strcmp(&newline[i][0],"<")==0)
+		        	{
+		        		array_arg[x-1][y]=NULL;
+		        		y=0;
+		        		x++;
+		        	}
+		        	else
+		        	{
+		        		array_arg[x-1][y]=newline[i];
+		        		y++;
+		        		
+		        	}
 
-        			}
-    
-        			
+    			}	    			
 			}
 			else if(number_of_in)
 			{
@@ -258,47 +239,39 @@ int main()
 				int status1;
 				char *newline[100];
 				char *array_arg[100][100];
-					//printf("input hai ye %s\n",cp);
 				char *line=linec;
-					for (i = 0;i<100; i++) {
-						newline[i]=strtok(line, delimit);
-						 if (newline[i]==NULL)
-						 	break;
-			            line=NULL;
-			            //printf("%s\n",newline[i]);
-			        }
-			        int x=1,y=0;
-			        for(i=0;newline[i]!=NULL;i++)
-			        {
-			        	//printf("%s\n",newline[i]);
-			        	if(strcmp(&newline[i][0],">")==0)
-			        	{
-			        		array_arg[x-1][y]=NULL;
-			        		y=0;
-			        		x++;
-			        		//printf("sdfsdfsf\n");
-			        	}
-			        	else
-			        	{
-			        		array_arg[x-1][y]=newline[i];
-			        		//printf("%d %d %s\n",x-1,y, array_arg[x-1][y]);
-			        		y++;
-			        		
-			        	}
-
-        			}
-        			pid_t pid;
-        			pid=fork();
-        			if(pid==0)
-        			{
-        				fp = fopen(array_arg[1][0], "a");
-						dup2(fileno(fp), 1);
-						execvp(array_arg[0][0], array_arg[0]);
-						break;
-					}
-					else
-						waitpid(pid, &status1, 0);
-
+				for (i = 0;i<100; i++) {
+					newline[i]=strtok(line, delimit);
+					 if (newline[i]==NULL)
+					 	break;
+		            line=NULL;
+		        }
+		        int x=1,y=0;
+		        for(i=0;newline[i]!=NULL;i++)
+		        {
+		        	if(strcmp(&newline[i][0],">")==0)
+		        	{
+		        		array_arg[x-1][y]=NULL;
+		        		y=0;
+		        		x++;
+		        	}
+		        	else
+		        	{
+		        		array_arg[x-1][y]=newline[i];
+		        		y++;
+		        	}
+    			}
+    			pid_t pid;
+    			pid=fork();
+    			if(pid==0)
+    			{
+    				fp = fopen(array_arg[1][0], "a");
+					dup2(fileno(fp), 1);
+					execvp(array_arg[0][0], array_arg[0]);
+					break;
+				}
+				else
+					waitpid(pid, &status1, 0);
 			}
 			else if(number_of_out)
 			{
@@ -306,71 +279,55 @@ int main()
 				int status1;
 				char *newline[100];
 				char *array_arg[100][100];
-					//printf("input hai ye %s\n",cp);
 				char *line=linec;
-					for (i = 0;i<100; i++) {
-						newline[i]=strtok(line, delimit);
-						 if (newline[i]==NULL)
-						 	break;
-			            line=NULL;
-			            //printf("%s\n",newline[i]);
-			        }
-			        int x=1,y=0;
-			        for(i=0;newline[i]!=NULL;i++)
-			        {
-			        	//printf("%s\n",newline[i]);
-			        	if(strcmp(&newline[i][0],"<")==0)
-			        	{
-			        		array_arg[x-1][y]=NULL;
-			        		y=0;
-			        		x++;
-			        		//printf("sdfsdfsf\n");
-			        	}
-			        	else
-			        	{
-			        		array_arg[x-1][y]=newline[i];
-			        		//printf("%d %d %s\n",x-1,y, array_arg[x-1][y]);
-			        		y++;
-			        		
-			        	}
+				for (i = 0;i<100; i++) {
+					newline[i]=strtok(line, delimit);
+					 if (newline[i]==NULL)
+					 	break;
+		            line=NULL;
+		        }
+		        int x=1,y=0;
+		        for(i=0;newline[i]!=NULL;i++)
+		        {
+		        	if(strcmp(&newline[i][0],"<")==0)
+		        	{
+		        		array_arg[x-1][y]=NULL;
+		        		y=0;
+		        		x++;
+		        	}
+		        	else
+		        	{
+		        		array_arg[x-1][y]=newline[i];
+		        		y++;
+		        	}
 
-        			}
-        			pid_t pid;
-        			pid=fork();
-        			if(pid==0)
-        			{
-						fp = fopen(array_arg[1][0], "r");
-						dup2(fileno(fp), 0);
-						//printf("sdfsfsfd\n");
-						execvp(array_arg[0][0], array_arg[0]);
-						break;
-					}
-					else
-						waitpid(pid, &status1, 0);
-
+    			}
+    			pid_t pid;
+    			pid=fork();
+    			if(pid==0)
+    			{
+					fp = fopen(array_arg[1][0], "r");
+					dup2(fileno(fp), 0);
+					execvp(array_arg[0][0], array_arg[0]);
+					break;
+				}
+				else
+					waitpid(pid, &status1, 0);
 			}
 			else
 			{
-				if(strcmp(line_new[0],"cd")==0)
-				{
-					//printf("transfering\n");
-			
-						if(line_new[1]==NULL)	
-						{
-							//printf("Home Directory\n");
-							chdir(basedir);
-
-						}
-						else
-						{
-							chdir(line_new[1]);
-						}
-					
-		
+				if(strcmp(line_new[0],"cd")==0){
+					if(line_new[1]==NULL)	
+					{
+						chdir(basedir);
+					}
+					else
+					{
+						chdir(line_new[1]);
+					}
 				}
 				else if(strcmp(line_new[0],"clear")==0)
 				{
-					
 					pid_t pid;
 					pid = fork();
 					if(pid==0)
@@ -386,7 +343,6 @@ int main()
 					{                       
 	 	    			pid = waitpid(pid, NULL, 0);   
 					}
-
 				}
 				else if(strcmp(line_new[0],"ls")==0)
 				{
@@ -423,58 +379,54 @@ int main()
 						{
 							char gline[128];
 							while(fgets(gline,sizeof gline,fp1)!=NULL)
-      						{
-         						fputs(gline,stdout); 
-      						}
-      						fclose(fp1);
+	  						{
+	     						fputs(gline,stdout); 
+	  						}
+	  						fclose(fp1);
 					
 					}
-       			}
-       			else if(strcmp(line_new[0],"help")==0)
-       			{
-       				printf("--------------Shell developed by Siddharth Singh(2014105)---------\n");
-       				printf("--------------Functionalities are listed below--------------------\n");
-       				printf("--------------cd--------------------------------------------------\n");
-       				printf("--------------clear-----------------------------------------------\n");
-       				printf("--------------exit------------------------------------------------\n");
-       				printf("--------------multiple piping-------------------------------------\n");
-       				printf("--------------help------------------------------------------------\n");
-       				printf("--------------redirections(one at a time)-------------------------\n");
+	   			}
+	   			else if(strcmp(line_new[0],"help")==0)
+	   			{
+	   				printf("--------------Shell developed by Siddharth Singh(2014105)---------\n");
+	   				printf("--------------Functionalities are listed below--------------------\n");
+	   				printf("--------------cd--------------------------------------------------\n");
+	   				printf("--------------clear-----------------------------------------------\n");
+	   				printf("--------------exit------------------------------------------------\n");
+	   				printf("--------------multiple piping-------------------------------------\n");
+	   				printf("--------------help------------------------------------------------\n");
+	   				printf("--------------redirections(one at a time)-------------------------\n");
 
-       			}
+	   			}
 				else
 				{
-					
 					if(strlen(line)!=0)
+					{
+						pid_t pid;
+						pid = fork();
+						if(pid==0)
 						{
-							pid_t pid;
-							pid = fork();
-							if(pid==0)
+							i = 0;
+							while(i < len)
 							{
-								i = 0;
-								while(i < len)
-								{
-									line_new[i] = strdup(line_new[i]);
-									i++;
-								}
-								execvp(line_new[0],line_new);
+								line_new[i] = strdup(line_new[i]);
+								i++;
 							}
-							else if (pid < 0)
-							{                  
-			    				printf( "Error performing!\n");    
-			    				exit(1);
-							}	
-							else
-							{                       
-			 	    			pid = waitpid(pid, NULL, 0);   
-							}	
+							execvp(line_new[0],line_new);
 						}
-					}	
+						else if (pid < 0)
+						{                  
+		    				printf( "Error performing!\n");    
+		    				exit(1);
+						}	
+						else
+						{                       
+		 	    			pid = waitpid(pid, NULL, 0);   
+						}	
+					}
+				}	
 			}
-
-
-       }
-		}while(1);
-	
+   		}
+	}while(1);
    return 0;
 }
